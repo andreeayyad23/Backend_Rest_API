3f2e2045571644fcfff92035c5f08a64b450ec32
Andreh,ayyad <andreh,ayad@gmail.com>
2024-12-10 22:20:46 +0300

    Updates

--- app/user/views.py
+++ app/user/views.py
@@ -23,3 +23,27 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         """Retrieve and return authenticated user"""
         return self.request.user
+    
+    def update(self, request, *args, **kwargs):
+        """Update the authenticated user"""
+        return super().update(request, *args, **kwargs)
+    
+    def partial_update(self, request, *args, **kwargs):
+        """Update the authenticated user"""
+        return super().partial_update(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+        """Delete the authenticated user"""
+        return super().destroy(request, *args, **kwargs)
+    
+    def retrieve(self, request, *args, **kwargs):
+        """Retrieve the authenticated user"""
+        return super().retrieve(request, *args, **kwargs)
+    
+    def list(self, request, *args, **kwargs):
+        """List the authenticated user"""
+        return super().list(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        """Update the authenticated user"""
+        return super().put(request, *args, **kwargs)
