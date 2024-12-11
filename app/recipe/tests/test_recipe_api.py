3b234010c16e43d66643c9b21fccfc4b7839020b
Andreh,ayyad <andreh,ayad@gmail.com>
2024-12-11 19:50:36 +0300

    Recipe Api

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,98 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from core.models import Recipe, Tag, Ingredient
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
+
+RECIPES_URL = reverse('recipe:recipe-list')
+
+def detail_url(recipe_id):
+    """Return recipe detail URL"""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
+
+def sample_tag(user, name='Main course'):
+    """Create and return a sample tag"""
+    return Tag.objects.create(user=user, name=name)
+
+def sample_ingredient(user, name='Cinnamon'):
+    """Create and return a sample ingredient"""
+    return Ingredient.objects.create(user=user, name=name)
+
+def sample_recipe(user, **params):
+    """Create and return a sample recipe"""
+    defaults = {
+        'title': 'Sample recipe title',
+        'time_minutes': 10,
+        'price': 5.00
+    }
+    defaults.update(params)
+    return Recipe.objects.create(user=user, **defaults)
+
+class AuthenticatedRecipeApiTests(TestCase):
+    """Test authenticated recipe API access"""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            '1Hx7t@example.com',
+            'testpass'
+        )
+        self.client.force_authenticate(self.user)
+
+    def tearDown(self):
+        Recipe.objects.all().delete()
+
+    def test_retrieve_recipes(self):
+        """Test retrieving a list of recipes"""
+        sample_recipe(user=self.user)
+        sample_recipe(user=self.user)
+
+        res = self.client.get(RECIPES_URL)
+
+        recipes = Recipe.objects.all().order_by('-id')
+        serializer = RecipeSerializer(recipes, many=True)
+
+        # Debug logs for troubleshooting
+        print("Response data:", res.data)
+        print("Expected data:", serializer.data)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        # Exclude 'id' from direct comparison
+        for actual, expected in zip(res.data, serializer.data):
+            actual.pop('id', None)
+            expected.pop('id', None)
+            self.assertEqual(actual, expected)
+
+    def test_recipe_list_limited_to_user(self):
+        """Test list of recipes is limited to authenticated user"""
+        user2 = get_user_model().objects.create_user(
+            'fn1Hx7t@example.com',
+            'testpass'
+        )
+        sample_recipe(user=user2)
+        sample_recipe(user=self.user)
+
+        res = self.client.get(RECIPES_URL)
+
+        recipes = Recipe.objects.filter(user=self.user)
+        serializer = RecipeSerializer(recipes, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), len(serializer.data))
+        self.assertEqual(res.data, serializer.data)
+
+    def test_view_recipe_detail(self):
+        """Test viewing a recipe detail"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
